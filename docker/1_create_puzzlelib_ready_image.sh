805644033d466b84d3ae8c2179dde821c999abf1
EPi <e.pileckis@yandex.ru>
2022-12-21 12:42:36 +0200

    If docker is going to be run as root, it should be built as root

--- docker/1_create_puzzlelib_ready_image.sh
+++ docker/1_create_puzzlelib_ready_image.sh
@@ -5,9 +5,9 @@ sudo apt update
 sudo apt install -y nvidia-container-toolkit nvidia-docker2
 
 # TEST TO CHECK IF GPU IS WORKING WITH NVIDIA (2 LINES BELOW), CAN SKIP
-docker pull nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04
+sudo docker pull nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04
 sudo docker run -it  --gpus all nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04 nvcc
 # SHOULD SEE OUPUT (it's ok, because we haven't provided input for nvcc):
 #    nvcc fatal   : No input files specified; use option --help for more information
 
-docker build -t puzzlelib .  # add sudo if later step does not work
+sudo docker build -t puzzlelib .  # add sudo if later step does not work
